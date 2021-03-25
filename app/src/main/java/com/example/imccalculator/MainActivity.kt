ed07a7d49c06434661c59c7e094cc909a4640c32
Matheus Sant'anna <matsnt1223@outlook.com>
2021-03-24 22:38:25 -0300

    Initial commit

---
+++ app/src/main/java/com/example/imccalculator/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.imccalculator
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doAfterTextChanged
+import androidx.core.widget.doOnTextChanged
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setListeners()
+    }
+    
+    private fun setListeners() {
+        
+        alturaEDT?.doAfterTextChanged { text ->
+            Toast.makeText(this, text.toString(), Toast.LENGTH_SHORT).show()
+        }
+        
+        pesoEDT?.doOnTextChanged{ text, start, before, count ->  }
+
+        btnCalcular?.setOnClickListener {
+        calcularIMC(pesoEDT.text.toString(), alturaEDT.text.toString())
+        }
+    }
+
+    private fun calcularIMC(peso: String, altura: String){
+        val peso = peso.toFloatOrNull()
+        val altura = altura.toFloatOrNull()
+
+        if ((peso != null) && (altura != null)){
+            val imc = peso / (altura * altura)
+            textView.text = "IMC = %.0f".format(imc)
+
+            var nome = null
+        }
+    }
+}
